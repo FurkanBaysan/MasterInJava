4717f9afabd99ea5ac8bbb1c931b3c38b008bd2a
FurkanBaysan <furkanbaysan1600166@gmail.com>
2021-10-23 16:55:13 +0300

    Variables Usage Part1

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,30 @@
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        //--Challenge--
+        System.out.println("Furkan Baysan Back in Town !");
+
+        System.out.println("------------");
+        System.out.println("--VARIABLES--");
+
+        int firstNumber = (10 + 5) + (2 * 10);// declaration statement
+
+        // --Challenge--
+        int secondNumber = 12;
+        int thirdNumber = firstNumber * 2;
+
+        //--Challenge--
+        System.out.println("firstNumber hold that data:"
+                + firstNumber);
+
+        int total = firstNumber + secondNumber + thirdNumber;
+        System.out.println("Sum of the first, second and third numbers:" + total);
+
+        // --Challenge--
+        int lastOne = 1000 - total;
+        System.out.println("Value of lastOne:" + lastOne);
+        
+
+    }
+}
