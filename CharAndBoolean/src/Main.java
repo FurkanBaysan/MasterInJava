f8d6b1d60c865f9661ba539c99fee3c68db0b0b5
FurkanBaysan <furkanbaysan1600166@gmail.com>
2021-10-26 21:39:55 +0300

    Char and Boolean Primitive Types

---
+++ CharAndBoolean/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("--Char and Boolean Primitive Types--");
+
+        char myChar = 'D';
+        char firstUnicodeOfChar = '\u0044';
+        System.out.println(myChar);
+        System.out.println(firstUnicodeOfChar);
+        char copyRightChar = '\u00A9';
+        System.out.println(copyRightChar);
+
+        boolean isLoggedIntoSystem = false;
+        boolean isCustomerOverTwentyOne = true;
+        // namely, variable name should be a question.
+
+
+    }
+}
