23c28df874e8eff737170b246bbef5fd1dde68e1
FurkanBaysan <furkanbaysan1600166@gmail.com>
2021-11-24 16:02:47 +0300

    Keywords and Expressions

---
+++ KeywordsAndExpressions/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // A mile is equal to 1.609344 kilometers
+        System.out.println("-- Miles to Kilometers Conversion --");
+        double kilometerCorrespondingOneMile = 1.609344;
+        double result = 100 * (kilometerCorrespondingOneMile);
+        System.out.println("Kilometer Corresponding Given Miles:" + result);
+
+        int score = 100;
+        if (score > 99) {
+            System.out.println("Congrats, you got the High Score!");
+            score = 0;
+        }
+    }
+}
