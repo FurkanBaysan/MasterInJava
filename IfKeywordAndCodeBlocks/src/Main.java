42b754f91e5ee48526abe4ec53c783725d7ae97e
FurkanBaysan <furkanbaysan1600166@gmail.com>
2021-11-30 08:43:41 +0300

    If Keyword and Conditional Blocks

--- IfKeywordAndCodeBlocks/src/Main.java
+++ IfKeywordAndCodeBlocks/src/Main.java
@@ -1,34 +1,78 @@
 public class Main {
-
+    static int finalScore = 0;
 
     public static void main(String[] args) {
-        System.out.println("-- Game Scenario --");
-        boolean gameOver = true;
-        int score = 2000;
-        int levelCompleted = 5;
-        int bonus = 100;
-        int finalScore = 0;
-        int secondFinalScore = 0;
+        System.out.println("\t" + "-- Game Scenario --");
 
-        if (score >= 5000) {
-            System.out.println("Score: >= 5000 so " + " " + "that means you're one of the best players");
-        } else if (score < 5000 && score > 1000) {
-            System.out.println("Score: 1000-5000 so" + " " +
-                    "that means you're an intermediate player ");
-        } else {
-            System.out.println("Score: < 1000" + " " +
-                    "so that means you need to improve yourself");
-        }
+        int highScore = calculateFinalScore(true, 800, 5, 100);
+        System.out.println("Final Score:" + highScore);
+        //calculateFinalScore(true, 800, 5, 100); -> if calculateFinalScore method is void, we would call it exactly like that.
+        determinePlayerLevelBasedOnScore();
+        System.out.println("Final Score:" + " " + calculateFinalScore(true, 10000, 8, 200));
+        //calculateFinalScore(true, 10000, 8, 200);
+        determinePlayerLevelBasedOnScore();
+
+        System.out.println("\t" + "-- Competition Results --");
+        int highScorePosition = calculateHighScorePosition(1500);
+        displayHighScorePosition("Furkan Baysan", highScorePosition);
+        highScorePosition = calculateHighScorePosition((1000));
+        displayHighScorePosition("Ali Anil Toprak", highScorePosition);
+        highScorePosition = calculateHighScorePosition(400);
+        displayHighScorePosition("Velihan Gozek", highScorePosition);
+        highScorePosition = calculateHighScorePosition(50);
+        displayHighScorePosition("Zeynep Gurel", highScorePosition);
+
+
+    }
+
+    public static int calculateFinalScore(boolean gameOver, int score, int levelCompleted, int bonus) {
         if (gameOver) {
             finalScore = score + (levelCompleted * bonus);
-            System.out.println("Final Score:" + finalScore);
-        }
-        score = 10000;
-        levelCompleted = 8;
-        bonus = 200;
-        if (gameOver) {
-            secondFinalScore = score + (levelCompleted * bonus);
-            System.out.println("Second Final Score:" + secondFinalScore);
+            return finalScore;
+        } else {
+            System.out.println("The Game is Running");
+            return -1;
         }
     }
+
+    public static void determinePlayerLevelBasedOnScore() {
+        if (finalScore >= 5000) {
+            System.out.println("Score is the Top Players Range");
+        } else if (finalScore > 1000) {
+            System.out.println("Score is in the Intermediate Players Range");
+        } else {
+            System.out.println("Score is in Low Players Range");
+        }
+    }
+
+    public static void displayHighScorePosition(String playerName, int position) {
+        System.out.println(playerName + " " + "managed to get into position" + " " +
+                position + " " + "on the high score table");
+    }
+
+    public static int calculateHighScorePosition(int playerScore) {
+        if (playerScore >= 1000) {
+            return 1;
+        } else if (playerScore >= 500) {
+            return 2;
+        } else if (playerScore >= 100) {
+            return 3;
+        } else {
+            return 4;
+        }
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
