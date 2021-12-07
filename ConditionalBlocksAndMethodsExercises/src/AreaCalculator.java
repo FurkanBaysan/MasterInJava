77924e6536580ba0c5e7abbe164abbde5464caa0
FurkanBaysan <furkanbaysan1600166@gmail.com>
2021-12-07 13:10:13 +0300

    Conditional Blocks and Methods Algorithms

--- ConditionalBlocksAndMethodsExercises/src/AreaCalculator.java
+++ ConditionalBlocksAndMethodsExercises/src/AreaCalculator.java
@@ -6,7 +6,6 @@ public class AreaCalculator {
         } else
             return -1;
     }
-
     public static double area(double x, double y) {
         if (x >= 0 && y >= 0) {
             return x * y;
