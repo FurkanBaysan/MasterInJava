d1420c42e284cdcb0e8221ad3781ba183a542569
FurkanBaysan <furkanbaysan1600166@gmail.com>
2021-12-12 22:04:47 +0300

    Control Flow Exercises

---
+++ ControlFlowStatementsExercises/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        NumberInWord.printNumberInWord(5);
+        System.out.println(NumberOfDaysInMonth.getDaysInMonth(7, 1997));
+    }
+}
--- ControlFlowStatementsExercises/src/NumberOfDaysInMonth.java
+++ ControlFlowStatementsExercises/src/NumberOfDaysInMonth.java
@@ -30,7 +30,3 @@ public class NumberOfDaysInMonth {
         }
     }
 }
-
-
-
-
