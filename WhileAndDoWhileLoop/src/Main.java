d78774bced884b3c4abf9db823ce10fde1543175
FurkanBaysan <furkanbaysan1600166@gmail.com>
2021-12-14 01:50:48 +0300

    While And Do-While Loop

---
+++ WhileAndDoWhileLoop/src/Main.java
@@ -0,0 +1,68 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("\t" + "--While Loop--");
+        int count = 1;
+        while (count != 6) {
+            System.out.println("Count Value:" + " " + count);
+            count++;
+        }
+        System.out.println("\t" + "--Slightly Different Version Of Above While Loop--");
+        count = 1;//Set the Count to 1, because after executing above loop  count will be set to 6.
+        while (true) {
+            if (count == 6) {
+                break;
+            }
+            System.out.println("Count Value:" + " " + count);
+            count++;
+        }
+        System.out.println("\t" + "-- Do-While Loop --");
+        count = 1;
+        do {
+            System.out.println("Count Value:" + " " + count);
+            count++;
+        } while (count != 6);
+        System.out.println("\t" + "-- Do-While Loop Break Usage --");
+        count = 6;
+        do {
+            System.out.println("Count Value:" + " " + count);
+            count++;
+            if (count > 20)
+                break;
+        } while (count != 6);
+        System.out.println("\t" + "--Challenge--");
+        int startNumber = 4;
+        int finishNumber = 20;
+        while (startNumber <= finishNumber) {
+            startNumber++;
+            if (!isEvenNumber(startNumber)) {
+                continue;
+            }
+            System.out.println("Even Number:" + " " + startNumber);
+        }
+        System.out.println("\t" + "--Challenge--");
+        startNumber = 4;
+        count = 0;
+        while (startNumber <= finishNumber) {
+            startNumber++;
+            if (!isEvenNumber(startNumber)) {
+                continue;
+            }
+            System.out.println("Even Number:" + " " + startNumber);
+            count = count + 1;
+            if (count == 5) {
+                break;
+            }
+        }
+        System.out.println("Total Even Numbers Found:" + " " + count);
+    }
+
+    public static boolean isEvenNumber(int number) {
+        if (number % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
