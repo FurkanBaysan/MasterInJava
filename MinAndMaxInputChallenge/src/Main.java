612a375f4b2646b1c8238a26b8fc4d140ea89df2
FurkanBaysan <furkanbaysan1600166@gmail.com>
2021-12-25 15:15:06 +0300

    Min and Max Challenge

---
+++ MinAndMaxInputChallenge/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int maxNumber = Integer.MIN_VALUE;
+        int minNumber = Integer.MAX_VALUE;
+        while (true) {
+            System.out.print("Enter Number:" + " ");
+            boolean isAnNumber = scanner.hasNextInt();
+            if (isAnNumber) {
+                int number = scanner.nextInt();
+
+                if (number > maxNumber) {
+                    maxNumber = number;
+                }
+                if (number < minNumber) {
+                    minNumber = number;
+                }
+            } else {
+                break;
+            }
+            scanner.nextLine();//handle end of line (enter key)
+        }
+        System.out.println("Maximum Number:" + " " + maxNumber);
+        System.out.println("Minimum Number:" + " " + minNumber);
+        scanner.close();
+    }
+}
